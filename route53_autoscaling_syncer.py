3f02fabc85dba1b30148170304c3c97f2c908e56
Mike Placentra <mplacentra@runads.com>
2015-10-06 15:10:56 -0400

    20 sec max unhealthy

--- route53_autoscaling_syncer.py
+++ route53_autoscaling_syncer.py
@@ -21,7 +21,7 @@ logger = logging.getLogger(__name__)
 
 last_success = None
 last_success_lock = Lock()
-max_seconds_healthy = 15
+max_seconds_healthy = 20
 
 
 class HealthCheckHTTPRequestHandler(BaseHTTPRequestHandler):
