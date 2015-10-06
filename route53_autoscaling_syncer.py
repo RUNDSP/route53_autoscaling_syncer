de7dd4ad58e736899518fdce8e0ad0214195b98c
Mike Placentra <mplacentra@runads.com>
2015-10-06 17:43:59 -0400

    fix: use threaded health check server

--- route53_autoscaling_syncer.py
+++ route53_autoscaling_syncer.py
@@ -6,6 +6,7 @@ import argparse
 import datetime
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import logging
+from SocketServer import ThreadingMixIn
 from threading import Lock, Thread
 import time
 
@@ -53,10 +54,14 @@ class HealthCheckHTTPRequestHandler(BaseHTTPRequestHandler):
         return None
 
 
+class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
+    pass
+
+
 def start_health_check_server(host, port):
     logging.basicConfig()
     logging.error('starting server')
-    server = HTTPServer((host, port), HealthCheckHTTPRequestHandler)
+    server = ThreadedHTTPServer((host, port), HealthCheckHTTPRequestHandler)
     server.serve_forever()
 
 
